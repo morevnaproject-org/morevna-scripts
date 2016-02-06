87cfafd3b061fe94946e57f995a2d5e3d8a6b423
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2016-02-06 21:31:58 +0600

    Modified version to allow use Papagayo importer with CGCookie FlexRig.
    Details: http://morevnaproject.org/2015/11/11/automatic-lipsync-animation-in-blender/

--- blender/io_import_lipSync_Importer.py
+++ blender/io_import_lipSync_Importer.py
@@ -14,16 +14,18 @@
 #  along with this program; if not, write to the Free Software Foundation,
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
+# TODO:
+# * Relative path to lipsync file
+#
 # ##### END GPL LICENSE BLOCK #####
 
 bl_info = {
     "name": "LipSync Importer & Blinker",
-    "author": "Yousef Harfoush - bat3a ;)",
-    "version": (0, 5, 1),
+    "author": "Yousef Harfoush - bat3a ;) / Konstantin Dmitriev",
+    "version": (0, 5, 2),
     "blender": (2, 70, 0),
     "location": "3D window > Tool Shelf",
-    "description": "Plots Moho (Papagayo, Jlipsync, Yolo) file "
-        "to frames and adds automatic blinking",
+    "description": "Plot Moho (Papagayo, Jlipsync, Yolo) file to frames and adds automatic blinking. Modified by Konstantin Dmitriev for Morevna Project to support Pose Libraries and CG Cookie Flex Rig",
     "warning": "",
     "wiki_url": "http://wiki.blender.org/index.php?title=Extensions:2.6/Py/"
         "Scripts/Import-Export/Lipsync Importer",
@@ -54,6 +56,213 @@ def blinker():
     for y in range(scn.blinkNm):
         frame = y * scn.blinkSp + int(random()*modifier)
         createShapekey('blink', frame)
+        
+# ----------- cg cookie flexrig support-------------------
+
+lastPhonemeIdx = -100
+
+def blinkerFlexRig():
+    
+    scn = bpy.context.scene
+    obj = bpy.context.object
+    
+    # saving current scene state
+    state_record = scn.tool_settings.use_keyframe_insert_auto
+    state_frame = scn.frame_current
+    state_poselib = obj.pose_library
+
+    if scn.regMenuTypes.enumBlinkTypes == '0':
+        modifier = 0
+    elif scn.regMenuTypes.enumBlinkTypes == '1':
+        modifier = scn.blinkMod
+    
+    
+    # activate pose library
+    obj.pose_library = bpy.data.actions[scn.eyesLib]
+    scn.tool_settings.use_keyframe_insert_auto = True
+    
+    # find which bones should be affected by pose library
+    bones_list=[]
+    for bone in bpy.data.actions[scn.eyesLib].groups.keys():
+        if len(bpy.data.actions[scn.eyesLib].groups[bone].channels)!=0:
+            bones_list.append(bone)
+    
+    # select bones
+    if len(bpy.context.selected_pose_bones) != 0:
+        bpy.ops.pose.select_all()
+    for bone_name in obj.data.bones.keys():
+        if bone_name in bones_list:
+            obj.data.bones[bone_name].select=True
+        else:
+            obj.data.bones[bone_name].select=False
+    
+    offst = scn.offset     # offset value
+    frmIn = scn.easeIn     # ease in value
+    frmOut = scn.easeOut   # ease out value
+    hldIn = scn.holdGap    # holding time value
+    
+    
+    
+    #creating keys with blinkNm count
+    for y in range(scn.blinkNm):
+        frame = y * scn.blinkSp + int(random()*modifier)
+        #createShapekey('blink', frame)
+        
+        # inserting the In key only when phonem change or when blinking
+        #if lastPhoneme!=phoneme or eval(scn.regMenuTypes.enumModeTypes) == 1:
+        #    addFlexRigKey(offst+frame-frmIn, phoneme)
+        
+        addFlexRigKey(offst+frame-frmIn, 'rest')
+        addFlexRigKey(offst+frame, 'blink')
+        addFlexRigKey(offst+frame+hldIn, 'blink')
+        addFlexRigKey(offst+frame+hldIn+frmOut, 'rest')
+
+    
+    # restoring current scene state
+    scn.tool_settings.use_keyframe_insert_auto = state_record
+    scn.frame_current = state_frame
+    obj.pose_library = state_poselib
+        
+def lipsyncerFlexRig():
+    # reading imported file & creating keys
+    obj = bpy.context.object
+    scene = bpy.context.scene
+    bone = bpy.context.active_pose_bone
+    
+    # saving current scene state
+    state_record = scene.tool_settings.use_keyframe_insert_auto
+    state_frame = scene.frame_current
+    state_poselib = obj.pose_library
+    
+    
+    offst = scene.offset     # offset value
+    skVlu = scene.skscale    # shape key value
+    
+    #in case of Papagayo format
+    if scene.regMenuTypes.enumFileTypes == '0' :
+        frmIn = scene.easeIn     # ease in value
+        frmOut = scene.easeOut   # ease out value
+        hldIn = scene.holdGap    # holding time value
+        
+    #in case of Jlipsync format or Yolo
+    elif scene.regMenuTypes.enumFileTypes == '1' :
+        frmIn = 1
+        frmOut = 1
+        hldIn = 0
+        
+    # activate pose library
+    obj.pose_library = bpy.data.actions[scene.phonemesLib]
+    scene.tool_settings.use_keyframe_insert_auto = True
+        
+    # find which bones should be affected by pose library
+    bones_list=[]
+    for bone in bpy.data.actions[scene.phonemesLib].groups.keys():
+        if len(bpy.data.actions[scene.phonemesLib].groups[bone].channels)!=0:
+            bones_list.append(bone)
+    
+    # select bones
+    if len(bpy.context.selected_pose_bones) != 0:
+        bpy.ops.pose.select_all()
+    for bone_name in obj.data.bones.keys():
+        if bone_name in bones_list:
+            obj.data.bones[bone_name].select=True
+        else:
+            obj.data.bones[bone_name].select=False
+    
+    
+    f=open(bpy.path.abspath(scene.fpath)) # importing file
+    f.readline() # reading the 1st line that we don"t need
+    
+    global lastPhonemeIdx
+    global lastPhoneme
+    lastPhoneme = []
+    #lastPhoneme.append("rest")
+    global prevFrame
+    prevFrame = -100
+    
+    for line in f:
+        # removing new lines
+        lsta = re.split("\n+", line)
+
+        # building a list of frames & shapes indexes
+        lst = re.split(":? ", lsta[0])# making a list of a frame & number 
+        frame = int(lst[0])
+        phoneme = lst[1]
+        
+        print("%s --> %s" % (frame,lst[1]))
+        
+        pl = obj.pose_library
+        
+        # inserting the In key only when phonem change or when blinking
+        #if lastPhoneme[-1]!=phoneme or eval(scene.regMenuTypes.enumModeTypes) == 1:
+        #    addFlexRigKey(offst+frame-frmIn, phoneme)
+        
+        # add rest position right before the first phoneme
+        #if  len(lastPhoneme) == 0:
+        #    addFlexRigKey(offst+frame-frmIn, "rest")
+        
+        if ( len(lastPhoneme)==0 or lastPhoneme[-1] == "rest" ) and frame-prevFrame > hldIn:
+            if phoneme != "rest":
+                addFlexRigKey(offst+frame-frmIn, "rest")
+                print("adding extra keyframe for REST position")
+        
+        if frame-prevFrame>=frmOut or frame-prevFrame==0:
+            
+            if len(lastPhoneme)>=2 and frame-prevFrame==0 and lastPhoneme[-2]==phoneme:
+                # avoid duplicating phonemes
+                pass
+            elif len(lastPhoneme)!=0 and lastPhoneme[-1] == "rest" and phoneme == "rest":
+                # don't insert double rest phonemes
+                pass
+            else:
+                addFlexRigKey(offst+frame, phoneme)
+                addFlexRigKey(offst+frame+hldIn, phoneme)
+                #addFlexRigKey(offst+frame+hldIn+frmOut, phoneme)
+                
+                lastPhoneme.append(phoneme)
+                prevFrame=frame
+        else:
+            addFlexRigKey(offst+frame+1, phoneme)
+            addFlexRigKey(offst+frame+1+hldIn, phoneme)
+            lastPhoneme.append(phoneme)
+            prevFrame=frame+1
+                
+            
+    
+    # restoring current scene state
+    scene.tool_settings.use_keyframe_insert_auto = state_record
+    scene.frame_current = state_frame
+    obj.pose_library = state_poselib
+
+                
+def addFlexRigKey(frame=0, pose=""):
+    
+    global lastPhonemeIdx
+    
+    bpy.context.scene.frame_current=frame
+    
+    obj = bpy.context.object
+    pl = obj.pose_library
+    
+    idx = pl.pose_markers.find(pose)
+    rest_idx = pl.pose_markers.find("rest")
+    
+    
+    
+    if idx == -1:
+        idx = pl.pose_markers.find("etc")
+    
+    if idx != -1:
+        if (idx != lastPhonemeIdx) or (idx == rest_idx):
+            print("Apply pose %s" % idx)
+            bpy.ops.poselib.apply_pose(pose_index=idx)
+            lastPhonemeIdx=idx
+        
+        else:
+            print("skipping phoneme: pose already set")
+    
+    
+
 
 # -----------code contributed by dalai felinto adds armature support modified by me-------------------
 
@@ -78,7 +287,7 @@ def lipsyncerBone():
 
     resetBoneScale(bone)
 
-    f=open(scene.fpath) # importing file
+    f=open(bpy.path.abspath(scene.fpath)) # importing file
     f.readline() # reading the 1st line that we don"t need
 
     for line in f:
@@ -157,7 +366,7 @@ def lipsyncer():
     obj = bpy.context.object
     scn = bpy.context.scene
 
-    f=open(scn.fpath) # importing file
+    f=open(bpy.path.abspath(scn.fpath)) # importing file
     f.readline() # reading the 1st line that we don"t need
 
     for line in f:
@@ -237,11 +446,15 @@ class btn_lipsyncer(bpy.types.Operator):
                 else: print("No shape keys")
 
             elif obj.type=="ARMATURE":
-                if 1:#XXX add prop test
-                    if scn.fpath!='': lipsyncerBone()
+                if scn.regMenuTypes.enumBoneMethodTypes == '0':
+                    if scn.fpath!='': lipsyncerFlexRig()
                     else: print ("select a Moho file")
-                else: print("Create Pose properties")
-
+                else:
+                    if 1:#XXX add prop test
+                        if scn.fpath!='': lipsyncerBone()
+                        else: print ("select a Moho file")
+                    else: print("Create Pose properties")
+                    
             else: print ("Object is not a mesh ot bone")
         else: print ("Select object")
         return {'FINISHED'}
@@ -266,6 +479,8 @@ class btn_blinker(bpy.types.Operator):
                             blinker()
                             #return
                 else: print("No shape keys")
+            elif obj.type=="ARMATURE":
+                blinkerFlexRig()
             else: print ("Object is not a mesh ot bone")
         else: print ("Select object")
         return {'FINISHED'}
@@ -291,6 +506,11 @@ class menuTypes(bpy.types.PropertyGroup):
                                  name = 'Choose Mode',
                                  default = '0')
 
+    enumBoneMethodTypes = EnumProperty(items =(('0', 'Pose Library',''),
+                                               ('1', 'Bone Rotation','')),
+                                 name = 'Method',
+                                 default = '0')
+                                 
 # drawing the user interface
 class LipSyncBoneUI(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
@@ -341,14 +561,18 @@ class LipSyncUI(bpy.types.Panel):
     newType.skscale = FloatProperty(description="Smoothing shape key values", min=0.1, max=1.0, default=0.8)
     newType.offset = IntProperty(description="Offset your frames", default=0)
 
-    newType.easeIn = IntProperty(description="Smoothing In curve", min=1, default=3)
-    newType.easeOut = IntProperty(description="Smoothing Out curve", min=1, default=3)
+    newType.easeIn = IntProperty(description="Smoothing In curve", min=1, default=2)
+    newType.easeOut = IntProperty(description="Smoothing Out curve", min=1, default=2)
     newType.holdGap = IntProperty(description="Holding for slow keys", min=0, default=0)
 
     newType.blinkSp = IntProperty(description="Space between blinks", min=1, default=100)
     newType.blinkNm = IntProperty(description="Number of blinks", min=1, default=10)
 
     newType.blinkMod = IntProperty(description="Randomzing keyframe placment", min=1, default=10)
+
+    newType.phonemesLib = StringProperty(default="Lib_Phonemes")
+    newType.eyesLib = StringProperty(default="Lib_Eyes")
+
 
     def draw(self, context):
 
@@ -398,8 +622,6 @@ class LipSyncUI(bpy.types.Panel):
                 split.prop(context.scene, "holdGap", "Hold Gap")
                 split.prop(context.scene, "easeOut", "Ease Out")
 
-                col.operator('lipsync.go', text='Plot Keys to the Timeline')
-
             # Jlipsync & Yolo panel
             elif scn.regMenuTypes.enumFileTypes == '1':
                 col.prop(context.scene, "fpath")
@@ -409,9 +631,16 @@ class LipSyncUI(bpy.types.Panel):
                 split = col.split(align=True)
                 split.label("Frame Offset :")
                 split.prop(context.scene, "offset")
-
-                col.operator('lipsync.go', text='Plot Keys to the Timeline')
-
+                
+            if obj.type == "ARMATURE":
+                col.separator()
+                col.row().prop(scn.regMenuTypes, 'enumBoneMethodTypes')
+                if scn.regMenuTypes.enumBoneMethodTypes == '0':
+                    col.prop(context.scene, "phonemesLib", "Pose Library")
+                col.separator()
+                
+            col.operator('lipsync.go', text='Plot Keys to the Timeline')
+        
         # the blinker panel
         elif scn.regMenuTypes.enumModeTypes == '1':
             # choose blink type
@@ -431,7 +660,6 @@ class LipSyncUI(bpy.types.Panel):
                 split.prop(context.scene, "easeOut", "Ease Out")
                 col.prop(context.scene, "blinkSp", "Spacing")
                 col.prop(context.scene, "blinkNm", "Times")
-                col.operator('blink.go', text='Add Keys to the Timeline')
 
             # Random panel
             elif scn.regMenuTypes.enumBlinkTypes == '1':
@@ -449,8 +677,14 @@ class LipSyncUI(bpy.types.Panel):
                 split.prop(context.scene, "blinkSp", "Spacing")
                 split.prop(context.scene, "blinkMod", "Random Modifier")
                 col.prop(context.scene, "blinkNm", "Times")
-                col.operator('blink.go', text='Add Keys to the Timeline')
-
+            
+            if obj.type == "ARMATURE":
+                col.separator()
+                if scn.regMenuTypes.enumBoneMethodTypes == '0':
+                    col.prop(context.scene, "eyesLib", "Pose Library")
+                col.separator()
+                
+            col.operator('blink.go', text='Add Keys to the Timeline')
 
 # clearing vars
 def clear_properties():
@@ -458,8 +692,9 @@ def clear_properties():
     # can happen on reload
     if bpy.context.scene is None:
         return
-
-    props = ["fpath", "skscale", "offset", "easeIn", "easeOut", "holdGap", "blinkSp", "blinkNm", "blinkMod"]
+     
+    props = ["fpath", "skscale", "offset", "easeIn", "easeOut", "holdGap", "blinkSp", "blinkNm", "blinkMod", "phonemesLib", "eyesLib"]
+    
     for p in props:
         if p in bpy.types.Scene.bl_rna.properties:
             exec("del bpy.types.Scene."+p)
