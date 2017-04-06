ad8669f920d11774e9023511c36ab340bec8afa8
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2017-04-06 13:23:35 +0700

    Blender's ReplacePaths: Now replace works for data inside of MetaStrips.

--- blender/ReplacePaths.py
+++ blender/ReplacePaths.py
@@ -11,26 +11,33 @@ def do_replace(oldpath, newpath):
     seq=bpy.data.scenes[0].sequence_editor.sequences_all
 
     for i in seq:
-        if i.type == 'SOUND':
-            if oldpath in i.sound.filepath:
-                p = i.sound.filepath.replace(oldpath, newpath)
-                print("Replacing %s to %s ..." % (i.sound.filepath, p))
-                i.sound.filepath=p
-        if i.type == 'MOVIE':
-            if oldpath in i.filepath:
-                p = i.filepath.replace(oldpath, newpath)
-                print("Replacing %s to %s ..." % (i.filepath, p))
-                i.filepath=p
-        if i.type=='IMAGE':
-            if oldpath in i.directory:
-                p = i.directory.replace(oldpath, newpath)
-                print("Replacing %s to %s ..." % (i.directory, p))
-                i.directory=p
-            for j in i.elements:
-                if oldpath in j.filename:
-                    p = j.filename.replace(oldpath, newpath)
-                    print("Replacing %s to %s ..." % (j.filename, p))
-                    j.filename=p
+        _replace(i, oldpath, newpath)
+
+
+def _replace(i, oldpath, newpath):
+    if i.type == 'SOUND':
+        if oldpath in i.sound.filepath:
+            p = i.sound.filepath.replace(oldpath, newpath)
+            print("Replacing %s to %s ..." % (i.sound.filepath, p))
+            i.sound.filepath=p
+    if i.type == 'MOVIE':
+        if oldpath in i.filepath:
+            p = i.filepath.replace(oldpath, newpath)
+            print("Replacing %s to %s ..." % (i.filepath, p))
+            i.filepath=p
+    if i.type=='IMAGE':
+        if oldpath in i.directory:
+            p = i.directory.replace(oldpath, newpath)
+            print("Replacing %s to %s ..." % (i.directory, p))
+            i.directory=p
+        for j in i.elements:
+            if oldpath in j.filename:
+                p = j.filename.replace(oldpath, newpath)
+                print("Replacing %s to %s ..." % (j.filename, p))
+                j.filename=p
+    if i.type=='META':
+        for j in i.sequences:
+            _replace(j, oldpath, newpath)
 
 
 do_replace(oldpath, newpath)
